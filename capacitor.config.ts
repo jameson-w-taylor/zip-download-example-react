2fa61a9937e596679f7d4f0156a604496552326e
Jameson Taylor <jameson@ionic.io>
2023-02-28 12:09:48 -0500

    match repo name with package.json

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'zip-download',
+  appName: 'zip-download-example-react',
   webDir: 'build',
   bundledWebRuntime: false
 };
