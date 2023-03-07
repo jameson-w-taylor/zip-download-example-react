3e99895c653cde215a2ac9cb976f1103c7dddbd0
Jameson Taylor <jameson@ionic.io>
2023-03-07 15:54:19 -0500

    Remove unneeded providers;

--- src/hooks/useFilesystem.ts
+++ src/hooks/useFilesystem.ts
@@ -1,15 +1,10 @@
-import { ReadFileOptions } from '@capacitor/filesystem';
-import { useContext } from 'react';
-import { FilesystemContext } from '../components/FilesystemProvider';
+import { Filesystem, ReadFileOptions } from '@capacitor/filesystem';
 
 export const useFilesystem = (options: Pick<ReadFileOptions, 'directory'|'encoding'>) => {
-  const { read, write } = useContext(FilesystemContext);
   const { directory, encoding } = options;
 
-  if (read === undefined || write === undefined) throw new Error('useFilesystem must be used within a FilesystemProvider');
-
   return {
-    read: (path: string) => read({ path, directory, encoding }),
-    write: (path: string, data: string) => write({ path, data, directory, encoding })
+    read: (path: string) => Filesystem.readFile({ path, directory, encoding }),
+    write: (path: string, data: string) => Filesystem.writeFile({ path, data, directory, encoding })
   };
 };
